07c709984a7aa2feaa5379a9aa2563de2891cabd
Yofan <yofan@stucel.com>
2023-07-25 21:59:10 +0700

    lessgouu

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="bg-neutral-50 text-orange-400"> Hallo</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
