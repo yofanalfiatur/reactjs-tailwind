ef0008ba3c2eb536ab4d421741b03b74578a34bc
Yofan <yofan@stucel.com>
2023-07-26 15:44:10 +0700

    test tailwind

---
+++ src/components/navbar.js
@@ -0,0 +1,5 @@
+export const NavBar = () => {
+    return (
+        <p className="p-6 text-green-400 bg-yellow-200 mx-auto max-w-5xl text-center m-5 hover:bg-slate-500 transition-all duration-500">testing dude</p>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Test from './App';
 import reportWebVitals from './reportWebVitals';
+import { NavBar } from './components/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar/>
+    <Test />
   </React.StrictMode>
 );
 
